c7393b9cd98ef79b2cfa2e400034456e17c81368
Sebastian Otalora <jhonsebas77@outlook.com>
2021-07-14 10:05:12 -0500

    Display the result in the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:river_pod_basic/src/app.dart';
 
 void main() => runApp(
-      const MyApp(),
+      ProviderScope(
+        child: const MyApp(),
+      ),
     );
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_pod_basic/src/features/jokes/logic/jokes_provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,10 +12,67 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('River Pod'),
         ),
-        body: const Center(
-          child: Text('River Pod Jokes'),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _JokesConsumer(),
+              SizedBox(
+                height: 50,
+              ),
+              _ButtonConsummer(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _JokesConsumer extends ConsumerWidget {
+  const _JokesConsumer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(
+    BuildContext context,
+    ScopedReader watch,
+  ) {
+    final state = watch(jokesNotifierProvider);
+    return state.when(
+      initial: () => Text('Toca para iniciar'),
+      loading: () => Center(
+        child: CircularProgressIndicator(),
+      ),
+      data: (joke) {
+        print('=====================');
+        print('[JOKE] -> $joke');
+        print('=====================');
+        return Text('${joke.setup} \n ${joke.delivery}');
+      },
+      error: (error) => Text('Error'),
+    );
+  }
+}
+
+class _ButtonConsummer extends ConsumerWidget {
+  const _ButtonConsummer({Key? key}) : super(key: key);
+  @override
+  Widget build(
+    BuildContext context,
+    ScopedReader watch,
+  ) {
+    final state = watch(jokesNotifierProvider);
+    return ElevatedButton(
+      child: Text('Press to get a Joke!'),
+      onPressed: !state.isLoading
+          ? () {
+              context.read(jokesNotifierProvider.notifier).getJoke();
+            }
+          : null,
     );
   }
 }
